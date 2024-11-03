09055a7c0f47d426a27318915d71b9044404bea9
Eric J <eric@roseandpoppie.com>
2024-11-03 16:56:09 -0500

    When creating projects for flags, don't also create an event

--- database/factories/FlagFactory.php
+++ database/factories/FlagFactory.php
@@ -18,7 +18,9 @@ class FlagFactory extends Factory
     public function definition(): array
     {
         $model = $this->faker->randomElement(['App\Models\Project', 'App\Models\User']);
-        $model = $model::factory()->create();
+        $model = $model::factory()->create([
+            'event_id' => null,
+        ]);
         return [
             'user_id' => User::factory(),
             'flaggable_type' => $model::class,
