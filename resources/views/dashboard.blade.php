532ceea30c301c27bab7082e7f08d1a9f1004551
Eric J <eric@roseandpoppie.com>
2025-10-30 21:21:06 -0400

    Fix logic on dashboard if has projects, but no active project

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
                 @include('users.following-card', ['users' => Auth::user()->following()->limit(5)->get()])
 
 
-                @if (Auth::user()->projects->count())
+                @if (Auth::user()->currentProject())
                     <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-grey-50 shadow">
                         <div class="px-4 py-5 sm:px-6 flex justify-between bg-white">
                             <h3 class="text-lg font-semibold leading-6 text-gray-900 dark:text-gray-200">Active Project</h3>
@@ -44,7 +44,7 @@
                         </div>
                         <div class="bg-gray-50 px-4 py-5 sm:p-6">
                             <div class="text-sm leading-5 text-gray-500 dark:text-gray-400">
-                                <p><a href="{{ route('projects.create') }}" class="text-blue-600">Create a new project</a> to get started.</p>
+                                <p><a href="{{ route('projects.create') }}" class="text-blue-600">Create a new project</a> or set set one of your <a href="{{ route('projects.index') }}" class="text-blue-600">existing projects</a> to active.</p>
                             </div>
                         </div>
                     </div>
